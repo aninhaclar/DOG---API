080e0ca7c941d97c5c7b4a437e06351137796a2f
ANA CLARA RIMUALDO PIRES <24122542@CFP127ALUNO.LOCAL>
2025-02-18 15:29:46 -0300

    CSS finalizado

---
+++ dogapi.js
@@ -0,0 +1,28 @@
+'use strict'
+
+async function pesquisarFotos(raca){
+    const url = `https://dog.ceo/api/breed/${raca}/images`
+
+    const response = await fetch(url)
+    const data = await response.json()
+    return data.message
+    // console.log (data.message)
+}
+
+function criarImagem(link){
+    const galeria = document.getElementById('galeria')
+    const novaImg = document.createElement('img')
+    novaImg.src = link
+
+    galeria.appendChild(novaImg)
+}
+async function preencherFotos (){
+  const raca = document.getElementById('raca').value
+  const fotos = await pesquisarFotos(raca)
+    // console.log(fotos)
+    
+    galeria.replaceChildren('')
+    fotos.forEach(criarImagem)
+}
+
+document.getElementById('pesquisar').addEventListener('click',preencherFotos)
